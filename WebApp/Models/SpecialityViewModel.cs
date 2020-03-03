799d259ef4c384e10e0f011036a03d8e94dd0e4b
Sushytska <valia.sushytska@gmail.com>
2020-03-03 14:09:22 +0200

    Remove ignored files

--- WebApp/Models/SpecialityViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp.Models
-{
-    public class SpecialityViewModel
-    {
-        public int Id { get; set; }
-        public string NameOfSpeciality { get; set; }
-        public string Department { get; set; }
-    }
-}
