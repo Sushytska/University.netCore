0c3dcf73c535be9c5bd8e88358842ee7aea9285f
Sushytska <valia.sushytska@gmail.com>
2020-03-10 22:15:53 +0200

    Added modal windows

--- BLL.Tests/DepartmentManagerTests.cs
+++ BLL.Tests/DepartmentManagerTests.cs
@@ -6,8 +6,10 @@ using DAL.Interfaces;
 using Entities.DTOModels;
 using Entities.Models;
 using Moq;
+//using NUnit.Framework;
 using Xunit;
 using Assert = Xunit.Assert;
+using NUnit.Framework.Constraints;
 
 
 namespace BLL.Tests
@@ -22,6 +24,7 @@ namespace BLL.Tests
             var mock = new Mock<IUnitOfWork>();
             mock.Setup(unitOfWork => unitOfWork.Departments.GetAll()).Returns(GetAllTestDepartmentDTO());
             var manager = new DepartmentManager(mock.Object, new AttributeValidator(), new Mapper(config));
+            DepartmentDTO departmentDto = new DepartmentDTO();
             
             // Act
             var result = manager.GetAll();
@@ -41,30 +44,70 @@ namespace BLL.Tests
             };
             return departments;
         }
+        // [Fact]
+        // public void GetByIdDepartment()
+        // {
+        //     // Arrange
+        //     var config = new MapperConfiguration(cfg => cfg.CreateMap<Department, DepartmentDTO>());
+        //     var mock = new Mock<IUnitOfWork>();
+        //     mock.Setup(unitOfWork => unitOfWork.Departments.Get(1)).Returns(GetByIdTestDepartmentDTO()[0]);
+        //     var manager = new DepartmentManager(mock.Object, new AttributeValidator(), new Mapper(config));
+        //
+        //     // Act
+        //     var result = manager.GetById(1);
+        //
+        //     // Arrange
+        //     var viewResult = Assert.IsType<List<DepartmentDTO>>(result);
+        //     var model = Assert.IsAssignableFrom<List<DepartmentDTO>>(viewResult);
+        //     Assert.Equal(GetByIdTestDepartmentDTO()[0].Id, model[0].Id);
+        // }
+        // private List<Department> GetByIdTestDepartmentDTO()
+        // {
+        //     var departments = new List<Department>
+        //     {
+        //         new Department {Id = 1, NameOfDepartment = "department", FacultyId = 1},
+        //         new Department {Id = 2, NameOfDepartment = "department", FacultyId = 1}
+        //     };
+        //     return departments;
+        // }
         [Fact]
-        public void GetByIdDepartment()
+         public void GetByIdDepartment()
+         {
+             // Arrange
+             var config = new MapperConfiguration(cfg => cfg.CreateMap<Department, DepartmentDTO>());
+             var mock = new Mock<IUnitOfWork>();
+             mock.Setup(unitOfWork => unitOfWork.Departments.Get(1)).Returns(GetByIdTestDepartmentDTO());
+             var manager = new DepartmentManager(mock.Object, new AttributeValidator(), new Mapper(config));
+        
+             // Act
+             var result = manager.GetById(1);
+        
+             // Arrange
+             var viewResult = Assert.IsType<DepartmentDTO>(result);
+             var model = Assert.IsAssignableFrom<DepartmentDTO>(viewResult);
+             Assert.Equal(GetByIdTestDepartmentDTO().Id, model.Id);
+         }
+         private Department GetByIdTestDepartmentDTO()
+         {
+             var departments = new Department {Id = 1, NameOfDepartment = "department", FacultyId = 1};
+             return departments;
+         }
+       
+        [Fact]
+        public void GetByIdDepartmentNotFound()
         {
             // Arrange
             var config = new MapperConfiguration(cfg => cfg.CreateMap<Department, DepartmentDTO>());
             var mock = new Mock<IUnitOfWork>();
-            mock.Setup(unitOfWork => unitOfWork.Departments.Get(1)).Returns(GetByIdTestDepartmentDTO());
+            mock.Setup(unitOfWork => unitOfWork.Departments.Get(3)).Returns(null as Department);
             var manager = new DepartmentManager(mock.Object, new AttributeValidator(), new Mapper(config));
         
             // Act
-            var result = manager.GetById(1);
+            var result = manager.GetById(3);
         
             // Arrange
-            var viewResult = Assert.IsType<DepartmentDTO>(result);
-            var model = Assert.IsAssignableFrom<DepartmentDTO>(viewResult);
-            Assert.Equal(GetByIdTestDepartmentDTO().Id, model.Id);
+            Assert.Null(result);
         }
-        private Department GetByIdTestDepartmentDTO()
-        {
-            var departments = new Department {Id = 1, NameOfDepartment = "department", FacultyId = 1};
-            return departments;
-        }
-       
-
        
     }
 }
